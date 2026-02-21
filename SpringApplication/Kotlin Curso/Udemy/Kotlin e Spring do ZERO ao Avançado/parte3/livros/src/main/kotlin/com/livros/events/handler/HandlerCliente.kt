b36c0583036f868e64bcda11b4e8ec4e34c8a099
Davi <davijuvinosouza@gmail.com>
2026-02-21 17:30:39 -0300

    .

--- SpringApplication/Kotlin Curso/Udemy/Kotlin e Spring do ZERO ao Avançado/parte3/livros/src/main/kotlin/com/livros/events/handler/HandlerCliente.kt
+++ SpringApplication/Kotlin Curso/Udemy/Kotlin e Spring do ZERO ao Avançado/parte3/livros/src/main/kotlin/com/livros/events/handler/HandlerCliente.kt
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Component
 @Component
 class HandlerCliente: HandlerEvent<AuditCliente>() {
         override fun process(event: AuditCliente) {
-
+            with(event.data) {
+                println("🛒 Compra processada:")
+                println("   ID: $id")
+                println("   Cliente: $nome")
+                println("   Email: $email ")
+                println("   Status: $status")
+            }
     }
 }
